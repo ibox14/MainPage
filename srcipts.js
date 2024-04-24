2f50b6b142a37cdd59dfc3df7678d67d917e175c
ibox14 <164636681+ibox14@users.noreply.github.com>
2024-04-24 21:52:39 +0200

    Add files via upload

--- srcipts.js
+++ srcipts.js
@@ -1,6 +1,75 @@
 <script src="ścieżka/do/scripts.js"></script>
 <script src="https://kit.fontawesome.com/2d10c1b7a7.js" crossorigin="anonymous"></script> // Biblioteka ikon FontAwesome.
 
+<!-- Structured Data -->
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "WINNG - Twój partner w esporcie",
+    "image": "",
+    "url": "https://ibox14.github.io/MainPage/",
+    "telephone": "",
+    "priceRange": "$$",
+    "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "",
+        "addressLocality": "",
+        "postalCode": "",
+        "addressCountry": "PL"
+    },
+    "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": "",
+        "longitude": ""
+    },
+    "openingHoursSpecification": [{
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday"
+        ],
+        "opens": "09:00",
+        "closes": "18:00"
+    },{
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Saturday",
+        "opens": "09:00",
+        "closes": "15:00"
+    }],
+    "sameAs": [
+        "https://www.facebook.com/",
+        "https://www.instagram.com/",
+        "https://www.youtube.com/"
+    ]
+}
+</script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CX932XHH7E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CX932XHH7E');
+</script>
+
+<!--  Hotjar Tracking Code for  -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:4958115,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
 // Przeszukuje linki do usług
 var serviceLinks = document.querySelectorAll('.service-link');
 
